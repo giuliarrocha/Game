fe43624e85941ba0e316561a3357d88754ebe55a
Giulia Rossatto Rocha <giulia.rocha@unesp.br>
2022-02-16 00:49:38 -0300

    Criação do interior da casa e importação de assets diferentes de móveis

---
+++ GameProject/Assets/Scripts/CustomCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Obtém a posição atual do cursor, variável do tipo Vector3
+        Vector3 position = Input.mousePosition;
+        // Ajusta posições x e y no objeto 3D, ignorando o z.
+        transform.position = new Vector3(
+        position.x + 12f, position.y - 12f
+        );
+    }
+}
---
+++ GameProject/Assets/Scripts/HideCursor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        // A visibilidade do cursor na tela vai ser habilitada se,
+        // e somente se, o usuário não ativar nenhum dos eixos de
+        // movimento (i.e. ficar parado)
+        Cursor.visible = !Input.GetButton("Horizontal") && !Input.GetButton("Vertical") && !Input.GetButton("Turn"); 
+        //Se não tiver Turn nos Inputs,
+        //usar no lugar de !Input.GetButton("Turn") o seguinte:
+        //!Input.GetButton(“Mouse x”)&&
+        //!Input.GetButton(“Mouse y”);
+    }
+}
