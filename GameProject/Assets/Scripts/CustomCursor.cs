dfc06e4b496fb8ca645bf4fd51111b026b6c1328
Giulia Rossatto Rocha <giulia.rocha@unesp.br>
2022-02-20 01:47:22 -0300

    Criação do cursor customizado e script Interactor para usar nos objetos

--- GameProject/Assets/Scripts/CustomCursor.cs
+++ GameProject/Assets/Scripts/CustomCursor.cs
@@ -12,11 +12,7 @@ public class CustomCursor : MonoBehaviour
 
     // Update is called once per frame
     void Update () {
-        // Obtém a posição atual do cursor, variável do tipo Vector3
         Vector3 position = Input.mousePosition;
-        // Ajusta posições x e y no objeto 3D, ignorando o z.
-        transform.position = new Vector3(
-        position.x + 12f, position.y - 12f
-        );
+        transform.position = new Vector3(position.x + 12f, position.y - 12f);
     }
 }
--- GameProject/Assets/Scripts/HideCursor.cs
+++ GameProject/Assets/Scripts/HideCursor.cs
@@ -11,13 +11,6 @@ public class HideCursor : MonoBehaviour
 
     void LateUpdate()
     {
-        // A visibilidade do cursor na tela vai ser habilitada se,
-        // e somente se, o usuário não ativar nenhum dos eixos de
-        // movimento (i.e. ficar parado)
         Cursor.visible = !Input.GetButton("Horizontal") && !Input.GetButton("Vertical") && !Input.GetButton("Turn"); 
-        //Se não tiver Turn nos Inputs,
-        //usar no lugar de !Input.GetButton("Turn") o seguinte:
-        //!Input.GetButton(“Mouse x”)&&
-        //!Input.GetButton(“Mouse y”);
     }
 }
