4a9f099a9a39e1b28ac68e5c95f1fe8448d982a8
larissabonadio <50805040+larissabonadio@users.noreply.github.com>
2022-02-27 17:13:33 -0300

    Criação do sistema de inventário

---
+++ GameProject/Assets/Scripts/Inventario.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventario : MonoBehaviour
+{
+
+    public RectTransform content;
+    public List<GameObject> itens;
+
+    void addItem()
+    {
+        if(content.childCount < itens.Count)
+        {
+            GameObject item = Instantiate(itens[content.childCount], content.position, Quaternion.identity) as GameObject;
+            item.transform.parent = content.transform;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        addItem();
+    }
+}
