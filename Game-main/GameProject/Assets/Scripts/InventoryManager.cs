6e6eeb60797076d3b184c8bc0793eda85a3ec385
larissabonadio <50805040+larissabonadio@users.noreply.github.com>
2022-02-28 22:38:05 -0300

    Sistema de inventário: Clica no objeto e ele aparece na barra do inventário

---
+++ Game-main/GameProject/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public List<Item> Itens = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        Itens.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        Itens.Remove(item);
+    }
+
+    public void ListItens()
+    {
+        foreach(Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in Itens)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<UnityEngine.UI.Text>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<UnityEngine.UI.Image>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+        }
+    }
+
+    public void Update()
+    {
+        ListItens();
+    }
+}
---
+++ Game-main/GameProject/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Item item;
+
+    void Pickup()
+    {
+        InventoryManager.Instance.Add(item);
+        Destroy(gameObject);
+    }
+
+    private void OnMouseDown()
+    {
+        Pickup();
+    }
+}
