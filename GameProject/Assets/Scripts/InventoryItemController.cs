8064d59017ae2fbdfcce70bd3ea7cca69875d381
larissabonadio <50805040+larissabonadio@users.noreply.github.com>
2022-03-05 20:35:53 -0300

    Inserindo novas assest e objetos

--- GameProject/Assets/Scripts/InventoryItemController.cs
+++ GameProject/Assets/Scripts/InventoryItemController.cs
@@ -16,42 +16,42 @@ public class InventoryItemController : MonoBehaviour
         item = newItem;
     }
 
-    public void UseItem() {
-        switch (item.itemType){
-            case Item.ItemType.Bituca:
-                Debug.Log("Bituca");
-                break;
-            case Item.ItemType.CaixaMadeira:
-                Debug.Log("Caixa");
-                break;
-            case Item.ItemType.Canudo:
-                Debug.Log("Canudo");
-                break;
-            case Item.ItemType.Comida:
-                Debug.Log("Comida");
-                break;
-            case Item.ItemType.GarrafaVidro:
-                Debug.Log("Garrafa Vidro");
-                break;
-            case Item.ItemType.Latinha:
-                Debug.Log("Latinha");
-                break;
-            case Item.ItemType.Papel:
-                Debug.Log("Papel");
-                break;
-            case Item.ItemType.Pet:
-                Debug.Log("Garrafa Pet");
-                break;
-            case Item.ItemType.Pilha:
-                Debug.Log("Pilha");
-                break;
-            case Item.ItemType.Remedio:
-                Debug.Log("Remedio");
-                break;
-            case Item.ItemType.Sacola:
-                Debug.Log("Sacola");
-                break;
-        }
+    public void UseItem() { // comentei pq da erro
+        // switch (item.itemType){
+        //     case Item.ItemType.Bituca:
+        //         Debug.Log("Bituca");
+        //         break;
+        //     case Item.ItemType.CaixaMadeira:
+        //         Debug.Log("Caixa");
+        //         break;
+        //     case Item.ItemType.Canudo:
+        //         Debug.Log("Canudo");
+        //         break;
+        //     case Item.ItemType.Comida:
+        //         Debug.Log("Comida");
+        //         break;
+        //     case Item.ItemType.GarrafaVidro:
+        //         Debug.Log("Garrafa Vidro");
+        //         break;
+        //     case Item.ItemType.Latinha:
+        //         Debug.Log("Latinha");
+        //         break;
+        //     case Item.ItemType.Papel:
+        //         Debug.Log("Papel");
+        //         break;
+        //     case Item.ItemType.Pet:
+        //         Debug.Log("Garrafa Pet");
+        //         break;
+        //     case Item.ItemType.Pilha:
+        //         Debug.Log("Pilha");
+        //         break;
+        //     case Item.ItemType.Remedio:
+        //         Debug.Log("Remedio");
+        //         break;
+        //     case Item.ItemType.Sacola:
+        //         Debug.Log("Sacola");
+        //         break;
+        // }
     }
 
 }
