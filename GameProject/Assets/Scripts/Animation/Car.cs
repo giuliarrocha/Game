b20884fa3d9c74d7ba34c62a078a8c702492ce2d
Giulia Rossatto Rocha <giulia.rocha@unesp.br>
2022-03-07 23:44:57 -0300

    Implementado: Jogar lixos no carro

--- GameProject/Assets/Scripts/Animation/Car.cs
+++ GameProject/Assets/Scripts/Animation/Car.cs
@@ -6,14 +6,56 @@ public class Car : MonoBehaviour
 {
     public Animator car;
     public Animator wheel1, wheel2, wheel3, wheel4;
+    public int totalItens = 0;
+    public GameManager m_Manager;
+
+    void Start() {
+        totalItens = 0;
+        GameObject controlCenter = GameObject.Find("ControlCenter");
+        m_Manager = controlCenter.GetComponent<GameManager>();
+    }
 
     void OnMouseDown()
     {
-        car.SetBool("moving", true);
-        wheel1.SetBool("moving", true);
-        wheel2.SetBool("moving", true);
-        wheel3.SetBool("moving", true);
-        wheel4.SetBool("moving", true);
+        bool andar = false;
+        bool guardaItemCarro = false;
+
+        //if (String.Equals(itensAchados.text,"27"))
+        if(m_Manager.segurandoObj){
+            if (m_Manager.idObj==3 || m_Manager.idObj==4 || m_Manager.idObj==8 || m_Manager.idObj==9){
+                //Colocar no carro Caixa de Madeira (3), Bituca (4), Remedio (8), Pilha (9)
+                //Carro so anda quando tiver os 4 tipos
+                totalItens++;
+                guardaItemCarro = true;
+                if (totalItens==4)
+                    andar = true; 
+                
+            }
+            else {
+                Debug.Log("Local incorreto para descarte do lixo.");
+                andar = false; 
+            }
+        }
+        else {
+            Debug.Log("Nao ha nada a ser jogado");
+            andar = false; 
+        }
+        
+        if(guardaItemCarro) {
+            Debug.Log("Item Guardado!");
+            m_Manager.segurandoObj = false;
+            Destroy(m_Manager.prefab);
+            m_Manager.atualizaItemStatus(1);
+        }
+        if (andar) {
+            car.SetBool("moving", true);
+            wheel1.SetBool("moving", true);
+            wheel2.SetBool("moving", true);
+            wheel3.SetBool("moving", true);
+            wheel4.SetBool("moving", true);
+        }
+
+
     }
 
     // private void OnTriggerExit(Collider other)
--- GameProject/Assets/Scripts/Item.cs
+++ GameProject/Assets/Scripts/Item.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -29,38 +29,3 @@ public class Item : ScriptableObject
         Papelao
     }
 }
-/*public Animator water;
-    public GameManager m_Manager;
-
-    void Start() {
-        GameObject controlCenter = GameObject.Find("ControlCenter");
-        m_Manager = controlCenter.GetComponent<GameManager>();
-    }
-    
-    void OnMouseDown()
-    {
-        bool abrir = false;
-        //if (String.Equals(itensAchados.text,"27"))
-        if(m_Manager.segurandoObj){
-            if (m_Manager.idObj==1 || m_Manager.idObj==6 || m_Manager.idObj==7){
-                //Se for latinha (1), garrafa de vidro (6) ou pet (7), pode lavar
-                abrir = true; 
-            }
-            else {
-                Debug.Log("Este lixo nao precisa ser lavado");
-                abrir = false; 
-            }
-        }
-        else {
-            Debug.Log("Nao ha nada a ser lavado");
-            abrir = false; 
-        }
-        
-        if(abrir) {
-            Debug.Log("Lavado!");
-            m_Manager.atualizaItemStatus(2);
-            water.SetBool("falling", true);
-
-            //tratar lixo
-        }
-    }*/
